c1217bcb6b8afa5a7b3b2b572eef2f5929c22e60
Sjur klemetsen <sjkl@nmbu.no>
2019-10-04 08:52:46 +0200

    Made test_sorting file
    Made the file and printed in the bubble_sort function

---
+++ src/sjur_klemetsen_ex/ex03_project/test_sorting.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Sjur Spjeld Klemetsen'
+__email__ = 'sjkl@nmbu.no'
+
+
+def bubble_sort(data):
+    datalist = list(data)
+    for i in range(1, len(datalist)):
+        for k in range(0, len(datalist) - i):
+            if datalist[k] > datalist[k + 1]:
+                datalist[k], datalist[k + 1] = datalist[k + 1], datalist[k]
+    return datalist
