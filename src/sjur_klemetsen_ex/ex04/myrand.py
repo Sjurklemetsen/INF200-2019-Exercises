35c3207ebf9691e4ac85af8afac124c7e71a9f17
Sjur klemetsen <sjkl@nmbu.no>
2019-10-22 11:05:39 +0200

    Started making LCGRand class and functions

---
+++ src/sjur_klemetsen_ex/ex04/myrand.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Sjur Klemetsen'
+__email__ = 'sjkl@nmbu.no'
+
+class LCGRand:
+
+    def _init__(self, seed):
+        self.a = 16807
+        self.m = 2^31 - 1
+        self.seed = None
+
+    def rand(self):
+        r = self.seed
+        while True:
+            r = (self.a * r % self.m)
+            yield r
+
+
+
+class ListRand:
+
+    def __init__(self):
