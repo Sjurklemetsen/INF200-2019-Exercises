8d16c328ff3b2cdbf4243f92ab774f8ff7721c80
Sjur klemetsen <sjkl@nmbu.no>
2019-10-22 11:11:58 +0200

    fixing the class

--- src/sjur_klemetsen_ex/ex04/myrand.py
+++ src/sjur_klemetsen_ex/ex04/myrand.py
@@ -3,11 +3,12 @@
 __author__ = 'Sjur Klemetsen'
 __email__ = 'sjkl@nmbu.no'
 
+
 class LCGRand:
 
     def _init__(self, seed):
         self.a = 16807
-        self.m = 2^31 - 1
+        self.m = 2 ^ 31 - 1
         self.seed = None
 
     def rand(self):
@@ -16,8 +17,7 @@ class LCGRand:
             r = (self.a * r % self.m)
             yield r
 
-
-
 class ListRand:
+# #def __init__(self):
 
-    def __init__(self):
+if __main__
