a99ce3fbf3a2983bdcb9f6e22ed28b215da4b934
Sjur klemetsen <sjkl@nmbu.no>
2019-10-24 12:49:30 +0200

    Made walker file and all the methods

---
+++ src/sjur_klemetsen_ex/ex04/walker.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Sjur Klemetsen'
+__email__ = 'sjkl@nmbu.no'
+
+import random as rd
+
+class Walker:
+    def __init__(self, pos, home):
+        self.pos = pos
+        self.home = home
+
+    def move(self):
+
+
+    def is_at_home(self):
+
+    def get_position(self):
+
+    def get_steps(self):
