164fb958e5215a98a5abf94610673fe1d154cc77
Sjur klemetsen <sjkl@nmbu.no>
2019-10-25 09:57:34 +0200

    Fixed ListRand class

--- src/sjur_klemetsen_ex/ex04/myrand.py
+++ src/sjur_klemetsen_ex/ex04/myrand.py
@@ -5,23 +5,30 @@ __email__ = 'sjkl@nmbu.no'
 
 
 class LCGRand:
-
     def __init__(self, seed):
         self.a = 16807
         self.m = 2**31 - 1
         self.seed = seed
 
     def rand(self):
-        while True:
-            self.seed = (self.a * self.seed) % self.m
-            return self.seed
+        self.seed = (self.a * self.seed) % self.m
+        return self.seed
 
 
 class ListRand:
-
     def __init__(self, numbers):
         self.numbers = numbers
+        self.counter = -1
 
     def rand(self):
-        for el in self.numbers:
-            yield el
+        if self.counter >= len(self.numbers) - 1:
+            raise RuntimeError
+        else:
+            self.counter += 1
+        return self.numbers[self.counter]
+
+
+if __name__ == "__main__":
+    x = LCGRand(346)
+    print(x.rand())
+    print(x.rand())
