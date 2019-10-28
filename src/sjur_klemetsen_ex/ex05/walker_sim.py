a1640cd44ba5ceb5a257a4de20e7ac656552bba5
Sjur klemetsen <sjkl@nmbu.no>
2019-10-28 13:49:45 +0100

    finished Simulation class

--- src/sjur_klemetsen_ex/ex05/walker_sim.py
+++ src/sjur_klemetsen_ex/ex05/walker_sim.py
@@ -37,70 +37,31 @@ class Walker:
 
 
 class Simulation:
-
     def __init__(self, start, home, seed):
-
         self.start = start
         self.home = home
         self.seed = seed
 
-        """
-        Initialise the simulation
-
-        Arguments
-        ---------
-        start : int
-            The walker's initial position
-        home : int
-            The walk ends when the walker reaches home
-        seed : int
-            Random generator seed
-        """
-
     def single_walk(self):
-        """
-                Simulate single walk from start to home, returning number of steps.
-
-                Returns
-                -------
-                int
-                    The number of steps taken
-                """
-        return Walker(0, 1).way_home()
-
+        rd.seed(self.seed)
+        return self.seed(Walker(self.start, self.home).way_home())
 
     def run_simulation(self, num_walks):
-        """
-        Run a set of walks, returns list of number of steps taken.
-
-        Arguments
-        ---------
-        num_walks : int
-            The number of walks to simulate
-
-        Returns
-        -------
-        list[int]
-            List with the number of steps per walk
-        """
-        return [Walker(0, 1).way_home() for i in range(num_walks)]
-
-
+        rd.seed(self.seed)
+        return [Walker(self.start, self.home).way_home()
+                for i in range(num_walks)]
 
 
 if __name__ == "__main__":
-    print(f' Distance : 1 -> Path lengths : '
-          f'{[Walker(0, 1).way_home() for i in range(5)]}')
-    print(f' Distance : 2 -> Path lengths :'
-          f' {[Walker(1, 2).way_home() for i in range(5)]}')
-    print(f' Distance : 5 -> Path lengths :'
-          f' {[Walker(1, 5).way_home() for i in range(5)]}')
-    print(f' Distance : 10 -> Path lengths :'
-          f' {[Walker(1, 10).way_home() for i in range(5)]}')
-    print(f' Distance : 10 -> Path lengths :'
-          f' {[Walker(1, 20).way_home() for i in range(5)]}')
-    print(f' Distance : 10 -> Path lengths :'
-          f' {[Walker(1, 50).way_home() for i in range(5)]}')
-    print(f' Distance : 10 -> Path lengths :'
-          f' {[Walker(1, 100).way_home() for i in range(6)]}')
-
+    print(f' 20 walks from start 0 to home 10 with seed 12345: '
+          f'{Simulation(0, 10, 12345).run_simulation(20)}')
+    print(f' 20 walks from start 0 to home 10 with seed 12345: '
+          f'{Simulation(0, 10, 12345).run_simulation(20)}')
+    print(f' 20 walks from start 10 to home 0 with seed 12345: '
+          f'{Simulation(10, 0, 12345).run_simulation(20)}')
+    print(f' 20 walks from start 10 to home 0 with seed 12345: '
+          f'{Simulation(10, 0, 12345).run_simulation(20)}')
+    print(f' 20 walks from start 0 to home 10 with seed 54321: '
+          f'{Simulation(0, 10, 54321).run_simulation(20)}')
+    print(f' 20 walks from start 10 to home 0 with seed 54321: '
+          f'{Simulation(10, 0, 54321).run_simulation(20)}')
