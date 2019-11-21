30515bb1defb8c60423ee56b130633858f90a31c
Sjur klemetsen <sjkl@nmbu.no>
2019-11-21 12:43:35 +0100

    Started making the tests
    Made docstrings for the tests arbitrary input works and the test if method moves you.

---
+++ src/sjur_klemetsen_ex/pa02/test_pa02.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Sjur Spjeld Klemetsen, Ola Flesche Hellenes'
+__email__ = 'sjkl@nmbu.no, olhellen@nmbu.no'
+
+from src.sjur_klemetsen_ex.pa02 import snakes_simulations as cs
+
+
+def test_arbitrary_input_works():
+    """
+    Test if your inputs are set as variables in board,
+     or if the standard variables are used anyway,
+      whether you give inputs or not.
+    :return:
+    """
+    b = cs.Board()
+    assert()
+
+def test_if_method_moves_you():
+    """
+    Test if the player moves a step
+     when the move method is called.
+    :return:
+    """
+    b = cs.Simulation.
+    assert()
+
